13f7ad217a75938d0807f5be591b32cd20bf903c
Guo Yanpei <73223812+GYPpro@users.noreply.github.com>
2023-11-05 16:53:11 +0800

    AC trie tree

--- Z_PIVE_TEST.cpp
+++ Z_PIVE_TEST.cpp
@@ -1,25 +1,9 @@
-#include <basic\subsearch.h>
-#include <math\other\binpow.h>
+// #include <basic\subsearch.h>
+// #include <math\other\binpow.h>
+#include <string\trie_Tree.h>
 #include <iostream>
 
 int main()
 {
-    for (;;)
-    {
-        int a,b;
-        cin >> a >> b;
-        cout << binpow(a,b) << endl;
-        // vector<ll int> a = testReadLine();
-        // for (;;)
-        // {
-        //     string s;
-        //     cin >> s;
-        //     ll int que = str2int(s);
-        //     if (que == INT64_MAX)
-        //     {
-        //         getchar(); break;
-        //     }
-        //     cout << subsearch_3<ll int>(que, a) << endl;
-        // }
-    }
+    
 }
---
+++ string/trie_Tree.h
@@ -0,0 +1,41 @@
+#include <template_overAll.h>
+
+
+class Trie//AC
+{
+public:
+    vector<map<char, int>> t;
+    int root = 0;
+    Trie()
+    {
+        t.resize(1);
+    }
+    void addedge(string _s)
+    {
+        int pvidx = root;
+        _s.push_back('-');
+        for (int i = 0; i < _s.size(); i++)
+        {
+            if (t[pvidx].find(_s[i]) != t[pvidx].end())
+            {
+                pvidx = t[pvidx][_s[i]];
+            }
+            else
+            {
+                t[pvidx][_s[i]] = t.size();
+                t.push_back(map<char, int>());
+                pvidx = t[pvidx][_s[i]];
+            }
+        }
+    }
+    bool ifcmp(string &s)
+    {
+        int pvidx = root;
+        for(int i = 0;i < s.size();i ++)
+        {
+            if(t[pvidx].find(s[i]) != t[pvidx].end()) pvidx = t[pvidx][s[i]];
+            else return 0;
+        }
+        return t[pvidx].find('-') != t[pvidx].end();
+    }
+};
