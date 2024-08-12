08ff52052584c6890330d852191c51c37507bf94
Guo Yanpei <73223812+GYPpro@users.noreply.github.com>
2024-08-12 18:06:58 +0800

    add Combination and Pollard-Rho

---
+++ 0_Template/math/number_theory/Comb.h
@@ -0,0 +1,34 @@
+#include <template_overAll.h>
+
+const int N = 1e6;
+const int mod = 1e9+7;
+
+int binpow(int x, int y)
+{
+    int ans = 1;
+    while (y)
+    {
+        if (y & 1) ans = ans * x % mod;
+        x = x * x % mod;
+        y >>= 1;
+    }
+    return ans;
+}
+
+vector<int> fac(N), inv(N);
+
+void init()
+{
+    fac[0] = inv[0] = 1;
+    for (int i = 1; i < N; i++) fac[i] = fac[i - 1] * i % mod;
+    inv[N - 1] = binpow(fac[N - 1], mod - 2);
+    for (int i = N - 2; i >= 1; i--)
+    {
+        inv[i] = inv[i + 1] * (i + 1) % mod;
+    }
+}
+
+auto C = [&](int x, int y) -> int
+{
+    return (fac[x] * inv[y] % mod) * inv[x - y] % mod;
+};
---
+++ 0_Template/math/number_theory/factor_pr.h
@@ -0,0 +1,175 @@
+#include <template_overAll.h>
+#define int long long
+#define pii pair<int, int>
+const int INF = 1145141919810LL;
+using namespace std;
+
+class Pollard_Rho
+{
+private:
+
+    vector<int> B;
+
+    int mul(int a, int b, int m)
+    {
+        int r = a * b - m * (int)(1.L / m * a * b);
+        return r - m * (r >= m) + m * (r < 0);
+    }
+
+    int mypow(int a, int b, int m)
+    {
+        int res = 1 % m;
+        for (; b; b >>= 1, a = mul(a, a, m))
+        {
+            if (b & 1)
+            {
+                res = mul(res, a, m);
+            }
+        }
+        return res;
+    }
+
+    bool MR(int n)
+    {
+        if (n <= 1)
+            return 0;
+        for (int p : B)
+        {
+            if (n == p)
+                return 1;
+            if (n % p == 0)
+                return 0;
+        }
+        int m = (n - 1) >> __builtin_ctz(n - 1);
+        for (int p : B)
+        {
+            int t = m, a = mypow(p, m, n);
+            while (t != n - 1 && a != 1 && a != n - 1)
+            {
+                a = mul(a, a, n);
+                t *= 2;
+            }
+            if (a != n - 1 && t % 2 == 0)
+                return 0;
+        }
+        return 1;
+    }
+
+    inline const int getfecsum(int _n)
+    {
+        int sum = 0;
+        while (_n)
+        {
+            sum += _n % 10;
+            _n /= 10;
+        }
+        return sum;
+    };
+
+    int PR(int n)
+    {
+        for (int p : B)
+        {
+            if (n % p == 0)
+                return p;
+        }
+        auto f = [&](int x) -> int
+        {
+            x = mul(x, x, n) + 1;
+            return x >= n ? x - n : x;
+        };
+        int x = 0, y = 0, tot = 0, p = 1, q, g;
+        for (int i = 0; (i & 255) || (g = gcd(p, n)) == 1; i++, x = f(x), y = f(f(y)))
+        {
+            if (x == y)
+            {
+                x = tot++;
+                y = f(x);
+            }
+            q = mul(p, abs(x - y), n);
+            if (q)
+                p = q;
+        }
+        return g;
+    }
+
+    vector<int> fac(int n)
+    {
+        // if(n == 0)
+        // #define pb emplace_back
+        if (n <= 1)
+            return {};
+        if (MR(n))
+            return {n};
+        int d = PR(n);
+        auto v1 = fac(d), v2 = fac(n / d);
+        auto i1 = v1.begin(), i2 = v2.begin();
+        vector<int> ans;
+        while (i1 != v1.end() || i2 != v2.end())
+        {
+            if (i1 == v1.end())
+            {
+                ans.pb(*i2++);
+            }
+            else if (i2 == v2.end())
+            {
+                ans.pb(*i1++);
+            }
+            else
+            {
+                if (*i1 < *i2)
+                {
+                    ans.pb(*i1++);
+                }
+                else
+                {
+                    ans.pb(*i2++);
+                }
+            }
+        }
+        return ans;
+    }
+
+public:
+
+    Pollard_Rho(){
+        B = {2, 3, 5, 7, 11, 13, 17, 19, 23};
+    }
+
+    vector<pii> fac_Comp(int n)
+    {
+        auto srt = fac(n);
+        map<int, int> cnt;
+        for (auto x : srt)
+            cnt[x]++;
+        vector<pii> rt;
+        for (auto x : cnt)
+            rt.push_back(x);
+        return rt;
+    }
+
+    vector<int> fac_pri(int n)
+    {
+        return fac(n);
+    }
+    
+    vector<int> fac_all(int n)
+    {
+        vector<pii> rt = fac_Comp(n);
+        vector<int> v;
+        function<void(int, int)> dfs = [&](int id, int x)
+        {
+            if (id == rt.size())
+            {
+                v.push_back(x);
+                return;
+            }
+            for(int i = 0;i <= rt[id].se;i ++)
+            {
+                dfs(id + 1, x * (mypow(rt[id].fi, i, INF)));
+            }
+        };
+        dfs(0, 1);
+        return v;
+    }
+};
