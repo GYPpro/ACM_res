ad565fa97c63f9e63ca5fa2ed74372fa09fd5ab5
Guo Yanpei <73223812+GYPpro@users.noreply.github.com>
2024-09-15 12:24:41 +0800

    1

--- 0_Template/graph/Tree/hld.h
+++ 0_Template/graph/Tree/hld.h
@@ -21,3 +21,31 @@ void dfs2(int o, int t) {
   for (int j = h[o]; j; j = nxt[j])
     if (p[j] != son[o] && p[j] != fa[o]) dfs2(p[j], p[j]);
 }
+
+int lca(int u, int v) {
+  while (top[u] != top[v]) {
+    if (dep[top[u]] > dep[top[v]])
+      u = fa[top[u]];
+    else
+      v = fa[top[v]];
+  }
+  return dep[u] > dep[v] ? v : u;
+} 
+
+// st 是线段树结构体
+int querymax(int x, int y) {
+  int ret = -inf, fx = top[x], fy = top[y];
+  while (fx != fy) {
+    if (dep[fx] >= dep[fy])
+      ret = max(ret, st.query1(1, 1, n, dfn[fx], dfn[x])), x = fa[fx];
+    else
+      ret = max(ret, st.query1(1, 1, n, dfn[fy], dfn[y])), y = fa[fy];
+    fx = top[x];
+    fy = top[y];
+  }
+  if (dfn[x] < dfn[y])
+    ret = max(ret, st.query1(1, 1, n, dfn[x], dfn[y]));
+  else
+    ret = max(ret, st.query1(1, 1, n, dfn[y], dfn[x]));
+  return ret;
+}
--- 0_Template/graph/Tree/lca.h
+++ 0_Template/graph/Tree/lca.h
@@ -1,31 +1,33 @@
 
 class LCA{ 
 public:
-    vector<vector<int>> cnj;
+    vector<vector<pii>> cnj;
     vector<int> lg,dep;
-    vector<array<int,32>> fa;
+    vector<array<int,32>> fa,wei;
     int n;
 
     LCA(int _n) {
         n = _n;
         cnj.resize(n+1);
-        lg.resize(n+1),fa.resize(n+1),dep.resize(n+1);
+        lg.resize(n+1),fa.resize(n+1),dep.resize(n+1),wei.resize(n+1);
         for(int i = 1; i <= n; i ++)
             lg[i] = lg[i-1] + (1 << lg[i-1] == i);
     }
 
-    void addEdge(int u,int v) {
-        cnj[u].push_back(v);
-        cnj[v].push_back(u);
+    void addEdge(int u,int v,int w) {
+        cnj[u].push_back({v,w});
+        cnj[v].push_back({u,w});
     }
 
     void build(int rt = 1) {
         using itf = function<void(int,int)>;
         itf dfs = [&](int p,int f) -> void {
             fa[p][0] = f,dep[p] = dep[f] + 1;
+            // wei[p][0] = 0;
             for(int i = 1;i <= lg[dep[p]];i ++) fa[p][i] = fa[fa[p][i-1]][i-1];
-            for(auto x:cnj[p]) if(x == f) continue;
-            else dfs(x,p);
+            for(int i = 1;i <= lg[dep[p]];i ++) wei[p][i] = max(wei[p][i-1],wei[fa[p][i-1]][i-1]);
+            for(auto [x,w]:cnj[p]) if(x == f) continue;
+            else wei[x][0] = w,dfs(x,p);
         };
         dfs(rt,0);
     }
@@ -37,5 +39,17 @@ public:
         for(int k = lg[dep[x]]-1;k >= 0;k --) if(fa[x][k] != fa[y][k]) x = fa[x][k],y = fa[y][k];
         return fa[x][0];
     }
+
+    int getmaxw(int x,int y) {
+        int curmx = 0;
+        if(dep[x] < dep[y]) swap(x,y);
+        while(dep[x] > dep[y]) curmx = max(curmx,wei[x][lg[dep[x] - dep[y]] - 1]), x = fa[x][lg[dep[x] - dep[y]] - 1];
+        if(x == y) return curmx;
+        for(int k = lg[dep[x]]-1;k >= 0;k --) 
+            if(fa[x][k] != fa[y][k]) 
+                curmx = max(curmx,wei[x][k]),x = fa[x][k],
+                curmx = max(curmx,wei[y][k]),y = fa[y][k];
+        return max({curmx,wei[x][0],wei[y][0]});
+    } 
 };
 
